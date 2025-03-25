f1a682177782580e39be0c83dce2cd24cfff5f29
ZenithSUS <jeranmerino147@gmail.com>
2025-03-25 16:25:13 +0800

    Refactor IP address handling by simplifying extraction logic and removing unused middleware

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import path from "path";
 import cors from "cors";
 import dotenv from "dotenv";
 import address from "./routes/address.js";
-import { ipDiagnosticMiddleware } from "./middleware/ip-address.js";
 import { fileURLToPath } from "url";
 import { logger } from "./middleware/logger.js";
 import { notFound } from "./middleware/not-found.js";
@@ -13,15 +12,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 dotenv.config();
-const PORT = 9000;
+const PORT = process.env.PORT || 9000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
-// Add this to your main server file
-app.set('trust proxy', true);
-
-// Middleware for comprehensive IP logging
-
+app.set("trust proxy", true);
 
 app.use(
   cors({
@@ -32,8 +27,8 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger);
-app.use(ipDiagnosticMiddleware)
 
+app.get("/favicon.ico", (req, res) => res.status(204).end());
 
 app.set("trust proxy", true);
 app.use("/api/addresses", address);
@@ -41,6 +36,6 @@ app.use("/api/addresses", address);
 app.use(notFound);
 app.use(error);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is listening on port ${PORT}`);
 });
