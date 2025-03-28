0beb5db0b0646aef7332a61fb8b03835e8e08d62
ZenithSUS <jeranmerino147@gmail.com>
2025-03-28 11:47:32 +0800

    Add test endpoint to retrieve various IP address headers for debugging

--- routes/address.js
+++ routes/address.js
@@ -6,4 +6,16 @@ const router = express.Router();
 router.get("/", insertAddress);
 router.get("/get-ip", getAddress)
 
+router.get("/test", async (req, res) => {
+    return res.status(200).json({
+        ...req.headers,
+        a: req.get("Remote-Addr"),
+        b: req.get("Client-IP"),
+        c: req.get("X-Forwared-For"),
+        d: req.get("X-Real-IP"),
+        e: req.ip,
+        h: req.socket.remoteAddress,
+    })
+})
+
 export default router;
